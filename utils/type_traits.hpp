4389902dc92896dbdedecd9f414622f619f35390
Curtis DAPURIFICACAO <cdapurif@e1r1p2.clusters.42paris.fr>
2022-09-21 18:23:55 +0200

    squeleton of is_integral based on boost library

--- utils/type_traits.hpp
+++ utils/type_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 16:09:27 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/09/07 15:38:36 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/09/21 18:22:23 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,32 @@
 
 namespace ft
 {
+    struct true_type {};
+    struct false_type {};
+    
     template<class T>
-    struct is_integral {};
+        struct is_integral {};
+    
+    template<>
+        struct is_integral<unsigned char> : public true_type {};
+    template<>
+        struct is_integral<unsigned short> : public true_type{};
+    template<>
+        struct is_integral<unsigned int> : public true_type{};
+    template<>
+        struct is_integral<unsigned long> : public true_type{};
+    template<>
+        struct is_integral<signed char> : public true_type{};
+    template<>
+        struct is_integral<short> : public true_type{};
+    template<>
+        struct is_integral<int> : public true_type{};
+    template<>
+        struct is_integral<long> : public true_type{};
+    template<>
+        struct is_integral<char> : public true_type{};
+    template<>
+        struct is_integral<bool> : public true_type{};
 }
 
 #endif
