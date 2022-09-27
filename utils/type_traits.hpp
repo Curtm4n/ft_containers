d3e44aa140fe31530bc662b342ad6780c47add8c
Curtis DAPURIFICACAO <cdapurif@e1r2p4.clusters.42paris.fr>
2022-09-27 16:38:40 +0200

    is_integral/integral_constant and enable_if finished

--- utils/type_traits.hpp
+++ utils/type_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 16:09:27 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/09/22 16:14:25 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/09/27 16:37:36 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,34 @@
 
 namespace ft
 {
-    struct true_type {};
-    struct false_type {};
+    template <class T, T v>
+        struct integral_constant
+        {
+            static const T                  value = v;
+            typedef T                       value_type;
+            typedef integral_constant<T,v>  type;
+            operator T()                    { return v; }
+        };
+
+    typedef integral_constant<bool,true>    true_type;
+    typedef integral_constant<bool,false>   false_type;
     
-    template<class T>
-        struct is_integral {};
+    template<class T>   struct is_integral                          : public false_type {};
     
-    template<>
-        struct is_integral<unsigned char> : public true_type {};
-    template<>
-        struct is_integral<unsigned short> : public true_type{};
-    template<>
-        struct is_integral<unsigned int> : public true_type{};
-    template<>
-        struct is_integral<unsigned long> : public true_type{};
-    template<>
-        struct is_integral<signed char> : public true_type{};
-    template<>
-        struct is_integral<short> : public true_type{};
-    template<>
-        struct is_integral<int> : public true_type{};
-    template<>
-        struct is_integral<long> : public true_type{};
-    template<>
-        struct is_integral<char> : public true_type{};
-    template<>
-        struct is_integral<bool> : public true_type{};
+    template<>          struct is_integral<unsigned char>           : public true_type {};
+    template<>          struct is_integral<unsigned short int>      : public true_type {};
+    template<>          struct is_integral<unsigned int>            : public true_type {};
+    template<>          struct is_integral<unsigned long int>       : public true_type {};
+    template<>          struct is_integral<unsigned long long int>  : public true_type {};
+    template<>          struct is_integral<signed char>             : public true_type {};
+    template<>          struct is_integral<short int>               : public true_type {};
+    template<>          struct is_integral<int>                     : public true_type {};
+    template<>          struct is_integral<long int>                : public true_type {};
+    template<>          struct is_integral<long long int>           : public true_type {};
+    template<>          struct is_integral<char>                    : public true_type {};
+    template<>          struct is_integral<wchar_t>                 : public true_type {};
+    template<>          struct is_integral<bool>                    : public true_type {};
+
 
     template<bool B, class T = void>
         struct enable_if {};
