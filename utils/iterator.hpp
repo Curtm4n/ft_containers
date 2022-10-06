67e7a730bf26da93a9da84b58be66a04bcb4b885
Curtis DAPURIFICACAO <cdapurif@e1r3p3.clusters.42paris.fr>
2022-10-06 17:53:36 +0200

    Add iterator.hpp with iterator_traits class for namespace ft

---
+++ utils/iterator.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 17:36:57 by cdapurif          #+#    #+#             */
+/*   Updated: 2022/10/06 17:52:38 by cdapurif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITERATOR_HPP
+# define ITERATOR_HPP
+
+namespace ft
+{
+    template <class Iterator>
+    class iterator_traits
+    {
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+        typedef typename Iterator::iterator_category    iterator_category;
+    };
+
+    template <class T>
+    class iterator_traits<T*>
+    {
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+        typedef typename Iterator::iterator_category    iterator_category;
+    };
+
+    template <class T>
+    class iterator_traits<const T*>
+    {
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+        typedef typename Iterator::iterator_category    iterator_category;
+    };
+}
+
+#endif
