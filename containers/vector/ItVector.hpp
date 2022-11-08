cffd59005e0cb3bb40ae9ca533d9b7587c466f68
Curtis DAPURIFICACAO <cdapurif@e3r9p7.clusters.42paris.fr>
2022-11-09 00:45:17 +0100

    ItVector seems working correctly now

--- containers/vector/ItVector.hpp
+++ containers/vector/ItVector.hpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 10:50:11 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/11/08 18:24:27 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/11/09 00:41:24 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define ITVECTOR_HPP
 
 # include <cstddef>
+# include <ostream>
 
 namespace ft
 {
@@ -32,7 +33,7 @@ namespace ft
 
             ItVector(pointer ptr = pointer()) : _ptr(ptr)                   {}
             ItVector(const ItVector<T>& x) : _ptr(x._ptr)                   {}
-            ItVector<T>&    operator=(const ItVector<T>& x)                 { this->_ptr = x._ptr; }
+            ItVector<T>&    operator=(const ItVector<T>& x)                 { this->_ptr = x._ptr; return (*this); }
             ~ItVector(void)                                                 {}
 
             //THIS ALLOW CONVERSION FROM ITERATOR TO CONST ITERATOR
@@ -41,18 +42,18 @@ namespace ft
                 return (ItVector<const T>(this->_ptr));
             }
 
-            pointer getPointer(void) const  {return (_ptr); }
+            pointer getPointer(void) const  { return (_ptr); }
             
             reference       operator*(void) const                   { return (*_ptr); }
             pointer         operator->(void) const                  { return (_ptr); }
-            ItVector<T>     operator+(difference_type x)            { return (_ptr + x); }
-            ItVector<T>     operator-(difference_type x)            { return (_ptr - x); }
+            ItVector<T>     operator+(difference_type n) const      { return (_ptr + n); }
+            ItVector<T>     operator-(difference_type n) const      { return (_ptr - n); }
             ItVector<T>&    operator++(void)                        { _ptr++; return (*this); }
             ItVector<T>&    operator--(void)                        { _ptr--; return (*this); }
-            ItVector<T>&    operator++(int)                         { ItVector<T>   tmp(*this); _ptr++; return (tmp._ptr); } //JUST CHANGE return (tmp) to return (tmp._ptr)
-            ItVector<T>&    operator--(int)                         { ItVector<T>   tmp(*this); _ptr--; return (tmp._ptr); }
-            void            operator+=(difference_type n)           { _ptr += n; }
-            void            operator-=(difference_type n)           { _ptr -= n; }
+            ItVector<T>     operator++(int)                         { ItVector<T>   tmp(*this); _ptr++; return (tmp); }
+            ItVector<T>     operator--(int)                         { ItVector<T>   tmp(*this); _ptr--; return (tmp); }
+            ItVector<T>&    operator+=(difference_type n)           { _ptr += n; return (*this); }
+            ItVector<T>&    operator-=(difference_type n)           { _ptr -= n; return (*this); }
             reference       operator[](difference_type n)           { return (_ptr[n]); }
 
         private:
@@ -67,7 +68,7 @@ namespace ft
         }
 
     template <class T, class U>
-        bool operator==(const ItVector<T>& x, const ItVector<U>& y)     //I HAVE TO DOUBLE CHECK THAT (should allow comparison between const and non const)
+        bool operator==(const ItVector<T>& x, const ItVector<U>& y)
         {
             return (x.getPointer() == y.getPointer());
         }
@@ -78,8 +79,20 @@ namespace ft
             return (x.getPointer() != y.getPointer());
         }
 
+    template <class T, class U>
+        bool operator!=(const ItVector<T>& x, const ItVector<U>& y)
+        {
+            return (x.getPointer() != y.getPointer());
+        }
+
     template <class T>
         bool operator<(const ItVector<T>& x, const ItVector<T>& y)
+        {
+            return (x.getPointer() < y.getPointer());
+        }
+
+    template <class T, class U>
+        bool operator<(const ItVector<T>& x, const ItVector<U>& y)
         {
             return (x.getPointer() < y.getPointer());
         }
@@ -90,8 +103,20 @@ namespace ft
             return (x.getPointer() > y.getPointer());
         }
 
+    template <class T, class U>
+        bool operator>(const ItVector<T>& x, const ItVector<U>& y)
+        {
+            return (x.getPointer() > y.getPointer());
+        }
+
     template <class T>
         bool operator<=(const ItVector<T>& x, const ItVector<T>& y)
+        {
+            return (x.getPointer() <= y.getPointer());
+        }
+
+    template <class T, class U>
+        bool operator<=(const ItVector<T>& x, const ItVector<U>& y)
         {
             return (x.getPointer() <= y.getPointer());
         }
@@ -102,10 +127,10 @@ namespace ft
             return (x.getPointer() >= y.getPointer());
         }
 
-    template <class T>
-        ItVector<T> operator+(const ItVector<T>& x, const ItVector<T>& y)
+    template <class T, class U>
+        bool operator>=(const ItVector<T>& x, const ItVector<U>& y)
         {
-            return (x.getPointer() + y.getPointer());
+            return (x.getPointer() >= y.getPointer());
         }
 
     template <class T>
@@ -115,16 +140,23 @@ namespace ft
         }
 
     template <class T>
-        ItVector<T> operator-(const ItVector<T>& x, const ItVector<T>& y)
+        typename ItVector<T>::difference_type operator-(const ItVector<T>& x, const ItVector<T>& y)
+        {
+            return (x.getPointer() - y.getPointer());
+        }
+
+    template <class T, class U>
+        typename ItVector<T>::difference_type operator-(const ItVector<T>& x, const ItVector<U>& y)
         {
             return (x.getPointer() - y.getPointer());
         }
 
     template <class T>
-        ItVector<T> operator-(typename ItVector<T>::difference_type x, const ItVector<T>& y)
-        {
-            return (x - y.getPointer());
-        }
+    std::ostream &  operator<<(std::ostream & o, const ItVector<T>& x)
+    {
+        o << x.getPointer();
+        return (o);
+    }
 }
 
 #endif
