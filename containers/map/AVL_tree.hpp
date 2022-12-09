bf10cc7f659d2a81bdbbe5b6992c20a238adf4e9
curtm4n <curtman.73@gmail.com>
2022-12-09 15:30:16 +0100

    Add struct node and node_base to AVL_tree.hpp and map iterators almost finished (only increment and decrement left to implement)

--- containers/map/AVL_tree.hpp
+++ containers/map/AVL_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 19:35:49 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/12/08 20:42:21 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/12/09 14:46:40 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,28 @@
 
 namespace ft
 {
+    struct node_base
+    {
+        typedef node_base*   base_ptr;
+
+        base_ptr    parent;
+        base_ptr    left;
+        base_ptr    right;
+    };
+
+    template <class Tp>
+    struct node : public node_base
+    {
+        typedef node<Tp>*   link_type;
+
+        Tp  data;
+
+        node(const Tp& val) : data(val) {}
+
+        Tp*         getDataPtr()        { return (&data); }
+        const Tp*   getDataPtr() const  { return (&data); }
+    };
+
     template <class Key, class Value, class KeyOfValue, class Compare, class Alloc = std::allocator<Value> >
     class AVL_tree
     {
--- containers/map/ItMap.hpp
+++ containers/map/ItMap.hpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 14:15:50 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/12/08 19:27:56 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/12/09 15:23:29 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,79 +14,56 @@
 # define ITMAP_HPP
 
 # include "../../utils/utility.hpp"
+# include "AVL_tree.hpp"
 
-# include <iostream>
 # include <cstddef>
 
 namespace ft
 {
-    template <class Key, class T>               //NEED FULL CHECK (PROBABLY FALSE, SIMPLE COPY OF MY ITVECTOR CHANGED TO BIDIRECTIONAL ITERATOR)
-    class ItMap
+    template <class Tp>
+    struct ItMap
     {
+        typedef Tp                              value_type;
+        typedef Tp*                             pointer;
+        typedef Tp&                             reference;
+        typedef std::ptrdiff_t                  difference_type;
+        typedef std::bidirectional_iterator_tag iterator_category;
 
-        public:
+        typedef ItMap<Tp>           Self;
+        typedef node_base::base_ptr base_ptr;
+        typedef node<Tp>*           link_type;
 
-            typedef pair<const Key, T>              value_type;
-            typedef value_type*                     pointer;
-            typedef value_type&                     reference;
-            typedef std::ptrdiff_t                  difference_type;
-            typedef std::bidirectional_iterator_tag iterator_category;
+        base_ptr    node;
 
-            ItMap(pointer ptr = pointer()) : _ptr(ptr)                  {}
-            ItMap(const ItMap<const Key, T>& x) : _ptr(x._ptr)          {}
-            ItMap<pair<const Key, T> >&    operator=(const ItMap<T>& x) { this->_ptr = x._ptr; return (*this); }
-            ~ItMap(void)                                                {}
+        ItMap() : node()            {}
+        ItMap(base_ptr x) : node(x) {}
 
-            //THIS ALLOW CONVERSION FROM ITERATOR TO CONST ITERATOR
-            operator ItMap<const Key, const T>() const
-            {
-                return (ItMap<const Key, const T>(this->_ptr));
-            }
+        reference   operator*()     { return(*(static_cast<link_type>(node)->getDataPtr())); }
+        pointer     operator->()    { return(static_cast<link_type>(node)->getDataPtr()); }
 
-            pointer base(void) const  { return (_ptr); }
-            
-            reference               operator*(void) const   { return (*_ptr); }
-            pointer                 operator->(void) const  { return (_ptr); }
-            ItMap<const Key, T>&    operator++(void)        { _ptr = _ptr->right; return (*this); }
-            ItMap<const Key, T>&    operator--(void)        { _ptr = _ptr->left; return (*this); }
-            ItMap<const Key, T>     operator++(int)         { ItMap<T>   tmp(*this); _ptr = _ptr->right; return (tmp); }
-            ItMap<const Key, T>     operator--(int)         { ItMap<T>   tmp(*this); _ptr = _ptr->left; return (tmp); }
+        /*Self&   operator++()
+        {
 
-        private:
+        }
 
-            pointer _ptr;
+        Self    operator++(int)
+        {
+
+        }
+
+        Self&   operator--()
+        {
+
+        }
+
+        Self    operator--(int)
+        {
+
+        }*/
+
+        friend bool operator==(const Self& x, const Self& y)    { return (x.node == y.node); }
+        friend bool operator!=(const Self& x, const Self& y)    { return (x.node != y.node); }
     };
-
-    template <class Key, class T>
-    bool operator==(const ItMap<const Key, T>& x, const ItMap<const Key, T>& y)
-    {
-        return (x.base() == y.base());
-    }
-
-    template <class Key, class T, class U>
-    bool operator==(const ItMap<const Key, T>& x, const ItMap<const Key, U>& y)
-    {
-        return (x.base() == y.base());
-    }
-
-    template <class Key, class T>
-    bool operator!=(const ItMap<const Key, T>& x, const ItMap<const Key, T>& y)
-    {
-        return (x.base() != y.base());
-    }
-
-    template <class Key, class T, class U>
-    bool operator!=(const ItMap<const Key, T>& x, const ItMap<const Key, U>& y)
-    {
-        return (x.base() != y.base());
-    }
-
-    template <class Key, class T>
-    std::ostream &  operator<<(std::ostream & o, const ItMap<const Key, T>& x)
-    {
-        o << x.base();
-        return (o);
-    }
 }
 
 #endif
