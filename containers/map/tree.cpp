69b1be7fb7c19cf4f65492304f070bda8549952c
Curtis DAPURIFICACAO <cdapurif@e3r9p7.clusters.42paris.fr>
2022-12-14 15:25:55 +0100

    left and right rotation implemented

--- containers/map/tree.cpp
+++ containers/map/tree.cpp
@@ -6,11 +6,13 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 11:43:55 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/12/13 17:55:47 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/12/14 15:20:47 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AVL_tree.hpp"
+
+#include <cassert>
 
 namespace ft
 {
@@ -168,11 +170,105 @@ namespace ft
     */
     void    rotate_left(node_base* x, node_base* & root)
     {
+        assert(x->right); //abort called if x->right is NULL (this function should never be called)
+        
+        node_base* y = x->right;
 
+        //change the child
+        x->right = y->left;
+        y->left = x;
+
+        //change the parent
+        y->parent = x->parent;
+        x->parent = y;
+
+        if (x->right) //this is b
+            x->right->parent = x;
+
+        //change the parent's child if it exist or change the root
+        if (x == root)
+            root = y;
+        else if (y->parent->right == x)
+            y->parent->right = y;
+        else
+            y->parent->left = y;
+
+        if (y->balFactor == 1)
+        {
+            /*
+            *       /                /
+            *      x                y
+            *     / \              / \
+            *    a   y      =>    x   c
+            *       / \          / \ /
+            *      b   c        a  b d
+            *         /
+            *        d
+            */
+           y->balFactor = 0;
+           x->balFactor = 0;
+        }
+        else
+        {
+            /*
+            *       /                /
+            *      x                y
+            *     / \              / \
+            *    a   y      =>    x   c
+            *       / \          / \
+            *      b   c        a   b
+            *       \               \
+            *        d               d
+            */
+           y->balFactor = -1;
+           x->balFactor = 1;
+        }
     }
 
+    /*       RIGHT ROTATION
+    *      /                /
+    *     x                y
+    *    / \      =>      / \
+    *   y   a            c   x
+    *  / \              / \
+    * c  [b]          [b]  a
+    */
     void    rotate_right(node_base* x, node_base* & root)
     {
-        
+        assert(x->left); //abort called if x->left is NULL (this function should never be called)
+
+        node_base* y = x->left;
+
+        //change the child
+        x->left = y->right;
+        y->right = x;
+
+        //change the parent
+        y->parent = x->parent;
+        x->parent = y;
+
+        if (x->left) //this is b
+            x->left->parent;
+
+        //change the parent's child if it exist or change the root
+        if (x == root)
+            root = y;
+        else if (y->parent->right == x)
+            y->parent->right = y;
+        else
+            y->parent->left = y;
+
+        if (y->balFactor == -1)
+        {
+            y->balFactor = 0;
+            x->balFactor = 0;
+        }
+        else
+        {
+            y->balFactor = 1;
+            x->balFactor = -1;
+        }
     }
+
+    //HERE
 }
